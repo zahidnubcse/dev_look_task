95676d48b25fc094027c249634dcd1e4849a5471
Zahid Hasan <128899625+zahidnubcse@users.noreply.github.com>
2026-05-07 23:23:44 +0600

    featured work

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeroSection from '../components/Hero'
 import Motivation from '../components/Motivation'
-// import FeaturedWork from '../components/FeaturedWork'
+import FeaturedWork from '../components/FeaturedWork'
 import Agency from '../components/Agency'
 import Services from '../components/Services'
 import Marquee_2 from '../components/Marquee_2'
@@ -15,7 +15,7 @@ export default function Home() {
      <HeroSection/>
      <Agency/>
      <Motivation/>
-     {/* <FeaturedWork/> */}
+     <FeaturedWork/>
      <Services/>
      <Marquee_2/>
      <ImageReveal/>
